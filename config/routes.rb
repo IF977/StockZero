548fd94c7afee4069a1e47218fa9bfa031b27da5
Lerisson Freitas <lff3@cin.ufpe.br>
2016-10-17 15:45:43 +0000

    Signup Feature done

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,19 @@
 Rails.application.routes.draw do
+  get 'sessinons/new'
+
+  get 'users/new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
-  get 'welcome/index'
+  # 'welcome/index'
   get 'login/login'
+  
+  get "/login" => "sessinons#new"
+  post "/login" => "sessinons#create"
+  delete "/logout" => "sessinons#destroy"
+  
+  get 'signup' => 'users#new'
+  
+  resources :users
   
 end
