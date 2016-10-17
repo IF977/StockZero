7c59d069fd71dc7c9a54308da8e2999ca980c4ec
Lerisson Freitas <lff3@cin.ufpe.br>
2016-10-17 15:07:42 +0000

    Commit to change branch

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
-  get 'sessions_controller/new'
+  resources :users
 
-  
+  get 'sessions_controller/new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'sessions#new'
-  #get 'users/new'
+  #get 'users/new' 
   #post 'users'
-  
+  get "log_out" => "sessions#destroy", :as => "log_out"
+  get "sign_in" => "sessions#new", :as => "sign_in"
+  resources :users
+  resources :sessions
 end
