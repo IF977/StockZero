255a4bbf9553812a3164b04f5f31a79e6b53bc95
Lerisson Freitas <lff3@cin.ufpe.br>
2016-10-31 13:57:36 +0000

    Edition of database

--- db/migrate/20161029182025_sorcery_core.rb
+++ db/migrate/20161029182025_sorcery_core.rb
@@ -2,8 +2,8 @@ class SorceryCore < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :email,            :null => false
-      t.string :crypted_password
-      t.string :salt
+      t.string :password
+      t.string :password_confirmation
 
       t.timestamps
     end
