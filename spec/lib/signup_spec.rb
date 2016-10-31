ac79c9693cf3a4f9c84af81a86f2950cc4287e4b
Lerisson Freitas <lff3@cin.ufpe.br>
2016-10-31 14:20:07 +0000

    BDD and TDD correction of BUG

--- spec/lib/signup_spec.rb
+++ spec/lib/signup_spec.rb
@@ -6,6 +6,7 @@ describe User do
     regex_email = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
     
     
+
     us=User.new
     
     it "Instance of user" do
@@ -14,7 +15,6 @@ describe User do
     
     it "e-mail is valid" do
         expect(us.email).to match(regex_email)
-        expect(us.email).not_to match(" ")
         expect(us.email).not_to be_empty
         expect(us.email).not_to match("@@.@_")
         expect(us.email).to be_an_instance_of(String)
@@ -22,8 +22,10 @@ describe User do
     it "password is valid" do
         expect(us.password).to match(regex_number) and match(regex_letters)
         expect(us.password).not_to be_empty
-        expect(us.password).not_to match("123456")
+        expect(us.password).not_to match("13253123")
         expect(us.password).not_to match("abcde")
-        
+       
     end
-end 
+end
+
+       
