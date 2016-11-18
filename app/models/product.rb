5d5b861204e46f0ec28cfdc2a97e236690ac0e04
Lerisson Freitas <lff3@cin.ufpe.br>
2016-11-18 19:56:00 +0000

    update validation products

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,22 @@ class Product < ActiveRecord::Base
   belongs_to :user
   has_attached_file :image, styles: { medium: "400x400>"}, default_url: "/assets/image_default.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+  validates_presence_of :nome_produto, message: ' Não pode ser deixado em branco'
+  validates_presence_of :preco, message: ' Não pode ser deixado em branco'
+  validates_presence_of :qtd_produtos, message: ' Não pode ser deixado em branco'
+  validates_presence_of :tempo_entrega, message: ' Não pode ser deixado em branco'
+  validates_presence_of :validade, message: ' Não pode ser deixado em branco'
+  validates_presence_of :min_qtd_entrega, message: ' Não pode ser deixado em branco'
+  validates_presence_of :descricao_produto, message: ' Não pode ser deixado em branco'
+  validates_presence_of :composicao_produto, message: ' Não pode ser deixado em branco'
+  validates_presence_of :categoria_produto, message: ' Não pode ser deixado em branco'
+  validates :nome_produto, length: { minimum: 30, message: ' Deve ter pelo menos 30 caracteres' }
+  validates :preco, length: { minimum: 6, message: ' Deve ter pelo menos 6 caracteres' }
+  validates :tempo_entrega, length: { minimum: 10, message: ' Deve ter pelo menos 10 caracteres' }
+  validates :validade, length: { minimum: 10, message: ' Deve ter pelo menos 10 caracteres' }
+  validates :min_qtd_entrega, length: { minimum: 1, message: ' Deve ter pelo menos 1 caracteres'}
+  validates :descricao_produto, length: { minimum: 140, message: ' Deve ter pelo menos 140 caracteres' }
+  
+   
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,9 @@ class User < ActiveRecord::Base
   validates :password_confirmation, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, length: { minimum: 6, message: 'Deve ter pelo menos 6 caracteres' }, if: -> { new_record? || changes[:crypted_password] }
   before_save { self.email = email.downcase }
-  validates :email, format: {with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: :create, message:"inválido deve ter '@', caracteres minúsculos e se faz necessário a presença do '.com'"}, uniqueness: {case_sensitive: false}
+  validates :email, format: {with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: :create, message:" Inválido deve ter '@', caracteres minúsculos e se faz necessário a presença do '.com'"}, uniqueness: {case_sensitive: false}
   validates :email, uniqueness: true
-  validates_presence_of :email, message: 'Não pode ser deixado em branco'
-  validates_presence_of :password, message: 'Não pode ser deixado em branco'
-  validates_presence_of :password_confirmation, message: 'Não pode ser deixado em branco'
+  validates_presence_of :email, message: ' Não pode ser deixado em branco'
+  validates_presence_of :password, message: ' Não pode ser deixado em branco'
+  validates_presence_of :password_confirmation, message: ' Não pode ser deixado em branco'
 end
