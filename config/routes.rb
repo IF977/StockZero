c4ffa3cbf6f4673c3c7d0400d97a4d5176c49a0c
Lerisson Freitas <lff3@cin.ufpe.br>
2016-11-28 22:57:15 +0000

    Help created

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root :to => 'users#index'
 resources :user_sessions
 resources :users
+get 'help' => 'help#help'
 
 get 'login' => 'user_sessions#new', :as => :login
 post 'logout' => 'user_sessions#destroy', :as => :logout
